0731003e31447777d2aba2286bdab014af0c1534
imberlyarboe <kimberjarboe37@gmail.com>
2019-11-19 17:13:03 -0600

    Add html template to index.html and start coding in app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+// Javascript data types: String, Integer, Boolean, Null
+
+console.log("hello world", "This is a string"); //String
+
+console.log(37, "This is an integer"); // Integer
+
+console.log(true, "This is a boolean"); // Boolean
+
+console.log(null, "This is null"); // Null
+
+// Javascript variables
+
+// A health bar in a video game is an example of a variable
+// Create a variable to represent life in a video game
+
+var life = 100;
+
+console.log(life);
+
+
+// Here we subtract 20 from the variable
+var newLife = life - 20
+
+
+// Here we console.log the new life
+console.log("Your life is now", newLife);
+
+var greeting = "Hello my name is";
+
+var name = "Gandalf";
+
+console.log(greeting, name);
+
+// An integer is any whole number
+console.log("This is an interger", 37);
+
+console.log(typeof(37));
+console.log(typeof(name));
